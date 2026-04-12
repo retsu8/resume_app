a04f3af5063f5a246869e65e2a6b16a8adcaba15
retsu8 <teck.geek1991@gmail.com>
2026-04-11 21:45:30 -0600

    now has base info and rule

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,46 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { Email, Phone } from "hidden-from-bots-react";
 import About from "./pages/about";
 import NavBar from "./pages/navbar"
 
 export default function Home() {
   return (
+    <>
     <ThemeProvider
       breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
       minBreakpoint="xxs">
       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <Container fluid>
-          <Row>
-            <Col>
-            <NavBar className="d-inline-block"/>
-          </Col>
+        <Container>
           <Col>
+            <Row>
+              <h1 className="text-info">William Paddock</h1>
+            </Row>
+            <Row>
+              <h4 className="text-light">React Developer</h4>
+            </Row>
+            <Row className="justify-content-md-left"> 
+                <Col md="auto">Belgrade, MT </Col>
+                <Col sm={1} > - </Col>
+                <Col md="auto"><Phone phone="406-855-1987">406-855-1987</Phone></Col>
+                <Col sm={1}> - </Col> 
+                <Col md="auto"><Email email="william.paddock08@gmail.com">william.paddock08@gmail.com</Email></Col>
+                <Col sm={1}> - </Col>
+                <Col md="auto"><a href="https://www.linkedin.com/in/wpaddock08/">linkedin.com/in/wpaddock08</a></Col>
+            </Row>
+            <Row>
+              <Col>
+                <h2 className="text-info">Experience</h2>
+              </Col>
+            </Row>
+            <Row>
+              <hr />
+            </Row>
           </Col>
-          </Row>
-          <Row>
-            <Col>William Paddock</Col>
-          </Row>
         </Container>
       </div>
     </ThemeProvider>
+    </>
     );
 }
--- app/pages/navbar.tsx
+++ app/pages/navbar.tsx
@@ -1,24 +1,12 @@
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function NavBar() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark" className="fixed-top">
-        <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item target="new tab" href="https://www.linkedin.com/in/wpaddock08/">LinkedIn</NavDropdown.Item>
-              <NavDropdown.Item target="new tab" href="https://github.com/retsu8">Github</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
+      <ListGroup bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
+          <ListGroup.Item href="#home">Home</ListGroup.Item>
+          <ListGroup.Item href="#home">About</ListGroup.Item>
+      </ListGroup>
       </>
     );
 }
